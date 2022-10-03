eeab300ba5e2dc740f5a41d4665fc9b881232da3
Vibi Sreenivasan <mail.vibi@gmail.com>
2022-10-03 12:59:44 +0200

    Add info function. Attempt to log to file, only if file is opened

--- mlog.py
+++ mlog.py
@@ -1,16 +1,25 @@
 import sys
 class log :
-    def __init__(self, debug):
+    def __init__(self, logfilename = None, debug = False):
         self.__debug_enabled = debug
-        self.__debug_file_name = sys.argv[0] + "_logfile.log"
-        self.__debug_file = open(self.__debug_file_name, "w")
+        if logfilename :
+            self.__debug_file_name = logfilename + "_logfile.log"
+            self.__debug_file = open(self.__debug_file_name, "w")
+        else :
+            self.__debug_file = None
     def __del__(self) :
-            self.__debug_file.close()
+            if self.__debug_file :
+                self.__debug_file.close()
     def err(self, *args) :
         print("ERR : ", *args)
         if self.__debug_file :
             print("ERR : ", *args, file=self.__debug_file)
     def dbg(self, *args):
-        print("DBG : ", *args)
         if self.__debug_enabled :
-            print("DBG : ", *args, file=self.__debug_file)
+            print("DBG : ", *args)
+            if self.__debug_file :
+                print("DBG : ", *args, file=self.__debug_file)
+    def info(self, *args) :
+        print("INF : ", *args)
+        if self.__debug_file :
+            print("INF : ", *args, file=self.__debug_file)
